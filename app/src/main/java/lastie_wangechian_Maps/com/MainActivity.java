f09d64ce63ce097b451c49642815a598af3bbc52
Mugo Simon <mugolastbon@gmail.com>
2021-01-02 15:45:59 +0300

    first commit. let's do it

---
+++ app/src/main/java/lastie_wangechian_Maps/com/MainActivity.java
@@ -0,0 +1,15 @@
+package lastie_wangechian_Maps.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
